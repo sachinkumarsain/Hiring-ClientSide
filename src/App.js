132546c442e3e9a1058fb70f872d34e63d83d529
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-23 23:27:13 +0530

    files set in route and file create

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import react from "react"
 import {BrowserRouter , Routes , Route} from "react-router-dom"
 
 import Home from "./Home/Home";
-
+import Category from "./category/Category";
+import Favourite from "./Favourite/Favourite";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 function App() {
   return (
     
    <>
 
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/category" element={<Category/>}/>
+    <Route path="/favourite " element={<Favourite />}/>
    </Routes>
+   <Footer/>
    </BrowserRouter>
 
    </>
