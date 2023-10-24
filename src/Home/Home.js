1f2c25930e4f2d1c6a5214cac6e0d39a5cdbbd36
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-24 17:52:26 +0530

    home file in user data on  action

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./Home.css"
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-import video from "../images/video.mp4"
+
+
+// import video from "../images/video.mp4"
 
 function Home() {
+
+  const navigate = useNavigate()
+
+  //.....................useForm.........................//
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+} = useForm();
+
+
+function onSubmit(data) {
+    console.log(data);
+
+    axios.post('http://localhost:8080/user', { data }, )
+        .then((result) => {
+            if (result.status === 200) {
+                console.log(result.data)
+                navigate("/category")
+            }
+            else if (result.status === 202) {
+                alert(result.data)
+            }
+        });
+}
   return (
     <>
 
@@ -12,6 +42,21 @@ function Home() {
           {/* <video className='videoTag' autoPlay loop muted>
             <source src={video} type='video/mp4' />
           </video> */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>name</label>
+            <input placeholder='enter Full Name'  {
+                        ...register('fullname', { required: true, })}></input>
+                        {errors.fullname && <p role="alert">Full Name is required</p>}
+
+
+            <label>Company Email</label>
+            <input placeholder='Enter Company Email' type='text'
+            {
+              ...register('companyEmail', { required: true, })}></input>
+            {errors.companyEmail && <p role="alert">Company Email is required</p>}
+
+            <button type='submit'>Submit</button>
+          </form>
         </section>
       </section>
     </>
