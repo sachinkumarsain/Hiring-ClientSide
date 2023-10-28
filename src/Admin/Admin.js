9af05a640d0e51efbf9c03a3e85080053a02d424
sachinkumarsain <sachinsain21082004@gmail.com>
2023-10-28 22:09:42 +0530

    admin file work

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import axios from "axios"
+import "./Admin.css"
 
 function Admin() {
+    const [tank, setTank] = useState([
+        { id: "1", value: "Full stack Developer" },
+        { id: "2", value: "Frontend" },
+        { id: "3", value: "Backend" },
+        { id: "4", value: "UI design" },
+        { id: "5", value: "UX design" }
+    ])
+    const [oneTank, setOneTank] = useState([])
+
+
+    function handleOne(e) {
+        if (e.target.checked === true) {
+
+            setOneTank([...oneTank,
+            e.target.value
+            ])
+        }
+        else {
+            setOneTank(oneTank.filter((list) => {
+                return list !== e.target.value
+            }))
+        }
+
+    }
+
+    console.log(oneTank)
+
+
 
     const {
         register,
@@ -14,11 +43,12 @@ function Admin() {
     function onSubmit(data) {
         console.log(data);
 
-        axios.post('http://localhost:8080/admin', { data }, 
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          }})
+        axios.post('http://localhost:8080/admin', { data, oneTank },
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            })
             .then((result) => {
                 if (result.status === 200) {
                     console.log(result.data)
@@ -30,7 +60,7 @@ function Admin() {
     }
     return (
         <>
-            <section>
+            <section className='admin'>
                 <form onSubmit={handleSubmit(onSubmit)} encType='multipart/form-data '>
                     <lebal>Full Name</lebal>
                     <input
@@ -47,12 +77,13 @@ function Admin() {
 
 
                     <lebal>Certificate</lebal>
-                    <select
+                    {/* 
+                     <select
                         {
                         ...register('course', { required: true, })}
                     >
                         <option>Select Game</option>
-                        <option>Full Stack Learning</option>
+                        <option>Full Stack developer</option>
                         <option>Frontend</option>
                         <option>Backend</option>
                         <option>UI design</option>
@@ -60,12 +91,33 @@ function Admin() {
                     </select>
 
 
-                    {errors.course && <p role="alert">course is required</p>}
+                    // {errors.course && <p role="alert">course is required</p>}  */}
+                    <lebal>Select feild</lebal>
+                    <ul style={{ listStyle: "none" }}
+
+
+                    >
+                        {
+                            tank.map((list, index) => {
+                                return <li key={index}>
+                                    <input type='checkbox' value={list.value} id={list.id} onChange={handleOne} />{list.value}
+                                </li>
+                            })
+                        }
+                    </ul>
+
+
+
 
 
                     <lebal>Email</lebal>
                     <input placeholder=' Enter email' type="email" {
-                        ...register('email', { required: true, })}></input>{errors.email && <p role="alert">email is required</p>}
+                        ...register('email', {
+                            required: true,
+                            pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+                            minLength: 8,
+                            maxLength: 60,
+                        })}></input>{errors.email && <p role="alert">email is required</p>}
 
                     <lebal>mobile</lebal>
                     <input type='number' placeholder='Enter mobile number'{
@@ -78,20 +130,84 @@ function Admin() {
 
                     <lebal >Linkedin</lebal>
                     <input placeholder='Enter Linkedin Link' type='text'{
-                        ...register('linkedin', { required: true, })}></input>{errors.linkedin && <p role="alert">linkedin is required</p>}
+                        ...register('linkedin', {
+                            required: true,
+                            pattern: /^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/,
+
+                        })}></input>{errors.linkedin && <p role="alert">linkedin is required</p>}
 
                     <lebal >Github</lebal>
                     <input placeholder=' Enter Github Link' {
-                        ...register('github', { required: true, })}></input>{errors.github && <p role="alert">gitub is required</p>}
+                        ...register('github', {
+                            required: true,
+                            pattern: /^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+\/?$/
 
+                        })}></input>{errors.github && <p role="alert">gitub is required</p>}
+
+                    {/* ...........................Profile(summary)............................. */}
 
                     <lebal>Enter summary</lebal>
                     <textarea type="text"{
                         ...register('summary', { required: true, })}></textarea>{errors.summary && <p role="alert">summary is required</p>}
 
-                    <h2>Skills</h2>  
+
+
+
+                    {/* ..........................Education................................... */}
+
+                    <h2>Education</h2>
+                    <h3>Graduation</h3>
+                    <lebal>StartYear</lebal>
+                    <input {...register('graStartYear', { required: true, })} type='text' placeholder='Enter Graduation  start year'></input>
+
+                    <lebal>LastYear</lebal>
+                    <input {...register('graLastYear', { required: true, })} type='text' placeholder='Enter Graduation  last year'></input>
+
+                    <lebal>Location</lebal>
+                    <input {...register('graLocation', { required: true, })} type='text' placeholder='Enter Location'></input>
+
+                    <lebal>Degree</lebal>
+                    <input {...register('graDegree', { required: true, })} type='text' placeholder='Enter Graduation Degree'></input>
+
+                    <lebal>University</lebal>
+                    <input {...register('university', { required: true, })} type='text' placeholder='Enter University Name'></input>
+
+                    <lebal>Description</lebal>
+                    <input {...register('graDec', { required: true, })} type='text' placeholder='Enter  description'></input>
 
                     <lebal>Skills</lebal>
+                    <input {...register('graSkills', { required: true, })} type='text' placeholder='Enter Skills'></input>
+
+
+                    {/* ....................................Intership................................... */}
+                    <h2>Intership</h2>
+                    <lebal>StartTime</lebal>
+                    <input {...register('intStartTime', { required: true, })} type='text' placeholder='Enter  Start time'></input>
+
+                    <lebal>complete Time</lebal>
+                    <input {...register('intComplete', { required: true, })} type='text' placeholder='Enter complete time'></input>
+
+                    <lebal>Location</lebal>
+                    <input {...register('intLocation', { required: true, })} type='text' placeholder='Enter Location'></input>
+
+                    <lebal>Degree</lebal>
+                    <input {...register('intDegree', { required: true, })} type='text' placeholder='Enter  Degree'></input>
+
+                    <lebal>Institute</lebal>
+                    <input {...register('institute', { required: true, })} type='text' placeholder='Enter Institute Name'></input>
+
+                    <lebal>Description</lebal>
+                    <input {...register('intDec', { required: true, })} type='text' placeholder='Enter  description'></input>
+
+                    <lebal>Skills</lebal>
+                    <input {...register('intSkills', { required: true, })} type='text' placeholder='Enter Skills'></input>
+
+
+                    {/* .....................................Skills................................ */}
+                    <h2>Skills</h2>
+
+                    <lebal>Skills</lebal>
+
                     <input placeholder='Enter Skills'{
                         ...register('skills', { required: true, })}></input>{errors.skills && <p role="alert">skills is required</p>}
 
@@ -100,7 +216,7 @@ function Admin() {
                     <input placeholder='Enter lanuages'{
                         ...register('lanuages', { required: true, })}></input>{errors.lanuages && <p role="alert"> lanuages is required</p>}
 
-                        <button type='submit'> submit</button>
+                    <button type='submit'> submit</button>
 
                 </form>
             </section>
